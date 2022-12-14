0b4e27fc2cfdf447092d1091d11d6cf7ce42d2d2
Martin Lizardi <martin.lizardi109@gmail.com>
2022-12-14 09:40:36 -0600

    useNavigate: historial de navegación

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { blogdata } from '../../data/blogdata';
 
 function BlogPost() {
+  const navigate = useNavigate();
   const { slug } = useParams();
 
   const blogpost = blogdata.find((post) => post.slug === slug);
 
+  const returnToBlog = () => {
+    navigate('/blog');
+    // navigate(-1);
+  };
+
   return (
     <>
       <h2>{blogpost.title}</h2>
+      <button onClick={returnToBlog}>Volver al blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
