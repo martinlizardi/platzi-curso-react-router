db68c2b9a84ab02b2d2c476d97709e31f2cd0679
Martin Lizardi <martin.lizardi109@gmail.com>
2022-12-14 13:48:24 -0600

    Menú con rutas públicas y privadas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../services/auth';
 
 function Menu() {
+  const auth = useAuth();
+
+  if (!auth.user) {
+  }
+
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({ color: isActive ? 'red' : 'blue' })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
+          if (route.publicOnly && auth.user) return null;
+          if (route.private && !auth.user) return null;
+
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive ? 'red' : 'blue' })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
@@ -24,22 +35,28 @@ const routes = [
   {
     to: '/',
     text: 'Home',
+    private: false,
   },
   {
     to: '/blog',
     text: 'Blog',
+    private: false,
   },
   {
     to: '/profile',
     text: 'Profile',
+    private: true,
   },
   {
     to: '/login',
     text: 'Login',
+    private: false,
+    publicOnly: true,
   },
   {
     to: '/logout',
     text: 'Logout',
+    private: true,
   },
 ];
 
